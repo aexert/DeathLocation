4fcd42c131563a1bb434b2f0a5a34be6abbdfaf8
Exe_rt <96650508+aexert@users.noreply.github.com>
2025-05-27 00:10:43 +0600

    Update DeathLocation.java

--- src/main/java/gg/aexert/deathlocation/DeathLocation.java
+++ src/main/java/gg/aexert/deathlocation/DeathLocation.java
@@ -3,40 +3,43 @@ package gg.aexert.deathlocation;
 import gg.aexert.deathlocation.commands.DLCommandManager;
 import gg.aexert.deathlocation.listeners.PlayerDeathListener;
 import gg.aexert.deathlocation.utils.ConfigManager;
+import gg.aexert.deathlocation.utils.CooldownManager;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathLocation extends JavaPlugin {
     private ConfigManager configManager;
+    private CooldownManager cooldownManager;
+
+    // ANSI escape code for purple (light magenta)
+    private static final String PURPLE = "\u001B[35m";
+    private static final String RESET = "\u001B[0m";
 
     @Override
     public void onEnable() {
-        // Console messages with standard colors
-        getLogger().info(ChatColor.GOLD + "DeathLocation " + ChatColor.DARK_GRAY + "• " +
-                ChatColor.WHITE + "Plugin by Aexert");
-        getLogger().info(ChatColor.GOLD + "DeathLocation " + ChatColor.DARK_GRAY + "• " +
-                ChatColor.WHITE + "Version 1.0.0");
-        getLogger().info(ChatColor.GOLD + "DeathLocation " + ChatColor.DARK_GRAY + "• " +
-                ChatColor.WHITE + "Loading configuration...");
+        getLogger().info(PURPLE + "DeathLocation • v1.1.0 by Aexert" + RESET);
+        getLogger().info(PURPLE + "DeathLocation • Loading configuration..." + RESET);
 
         this.configManager = new ConfigManager(this);
+        this.cooldownManager = new CooldownManager(this);
         configManager.loadConfig();
 
         Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(this), this);
         new DLCommandManager(this);
 
-        getLogger().info(ChatColor.GOLD + "DeathLocation " + ChatColor.DARK_GRAY + "• " +
-                ChatColor.WHITE + "Plugin enabled successfully!");
+        getLogger().info(PURPLE + "DeathLocation • Plugin enabled successfully!" + RESET);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(ChatColor.GOLD + "DeathLocation " + ChatColor.DARK_GRAY + "• " +
-                ChatColor.WHITE + "Plugin disabled");
+        getLogger().info(PURPLE + "DeathLocation • Plugin disabled" + RESET);
     }
 
     public ConfigManager getConfigManager() {
         return configManager;
+    }
+
+    public CooldownManager getCooldownManager() {
+        return cooldownManager;
     }
 }
